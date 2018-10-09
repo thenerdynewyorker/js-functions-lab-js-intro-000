1d0fbafde3d70fd7c0af490e5a4efddcd36c5c02
Samantha Mellone <samanthamellone@gmail.com>
2018-10-09 02:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,3 @@
 function happyHolidays (){
   return("Happy holidays!")
 }
-function happyHolidaysTo(name){
-  return(`Happy Holidays, ${name}!`)
-}
-function happyHolidayTo(holiday, name){
-  return{`Happy ${holiday}, ${name}!`}
-}
